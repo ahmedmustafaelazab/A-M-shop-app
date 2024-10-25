d94e71075a04da7df73c713e3769df3cc2ec4539
ahmedmustafaelazab <ahmedelazab511@gmail.com>
2024-10-25 14:38:26 +0300

    first commit

--- lib/modules/Screens/payment_screen/add_creditcard.dart
+++ lib/modules/Screens/payment_screen/add_creditcard.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/layout/layout_cubit/layout_cubit.dart';
-import 'package:shop_app/main.dart';
 import 'package:shop_app/shared/style/colors.dart';
 
 class AddCreditcard extends StatelessWidget {
@@ -17,15 +16,7 @@ class AddCreditcard extends StatelessWidget {
           children: [
             const Spacer(flex: 2),
             Image.network("https://i.postimg.cc/Gm9rj7FL/g10.webp"),
-            const Spacer(flex: 3),
-            Text(
-              "The amount deducted is:",
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
+            const Spacer(flex: 2),
             Text(
               "${cubit.cartData.map((e) => e.price).reduce((value, element) => value + element)} \$",
               textAlign: TextAlign.center,
@@ -34,23 +25,74 @@ class AddCreditcard extends StatelessWidget {
                   .headlineSmall!
                   .copyWith(fontWeight: FontWeight.bold),
             ),
-            const Spacer(flex: 3),
-            TextButton.icon(
+            Text(
+              "will be deducted from your account",
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            const Spacer(flex: 1),
+            ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (context) => MyApp()));
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.network(
+                              "https://i.postimg.cc/Y05yJcnF/yy.webp"),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            "Payment Done Successfully",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      actions: [
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Close the dialog
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  mainColor, // Set the background color to mainColor
+                            ),
+                            child: const Text(
+                              "Home",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color:
+                                    thirdColor, // Set the text color to thirdColor
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
               },
-              icon: const Text(
-                "Back to home",
-                style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: mainColor),
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    mainColor, // Set the background color to mainColor
               ),
-              label: const Icon(
-                Icons.home_repair_service,
-                size: 30,
-                color: mainColor,
+              child: const Text(
+                "Pay Now",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: thirdColor, // Set the text color to thirdColor
+                ),
               ),
             ),
             const Spacer(),
